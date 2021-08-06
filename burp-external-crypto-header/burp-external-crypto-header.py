0503cbbd5751daf2a77ab3cbd5c0b1b0755a7a62
Jimmy <jimmy-ly00@users.noreply.github.com>
2021-08-06 22:18:31 +0800

    Update burp-external-crypto-header.py

--- burp-external-crypto-header/burp-external-crypto-header.py
+++ burp-external-crypto-header/burp-external-crypto-header.py
@@ -2,19 +2,17 @@ from burp import IBurpExtender
 """
 Name:           External Crypto Header
 Version:        0.0.1
-Date:           27/04/2021
+Date:           10/03/2021
 Author:         Jimmy Ly
 Github:         https://github.com/jimmy-ly00
 
 Description:    This plugin adds headers useful for XXX
-                To run cryptographic functions and overcome Jython issues with calling built-in C 
-                libraries (e.g. Crypto). It calls an external (Python) program which removes the 
-                limitation of requiring built-in C libraries and pip installed tools.
 """
 
 from burp import ISessionHandlingAction
 from burp import IParameter
 from java.io import PrintWriter
+import hashlib
 
 import time
 import subprocess
@@ -26,7 +24,7 @@ class BurpExtender(IBurpExtender, ISessionHandlingAction):
 
   def run_external(self, payload):
     # https://github.com/externalist/aes-encrypt-decrypt-burp-extender-plugin-example
-    proc = subprocess.Popen(['python','./encrypt.py', self.key, payload],stdout=subprocess.PIPE)
+    proc = subprocess.Popen(['python','./sign.py', self.key, payload],stdout=subprocess.PIPE)
     output = proc.stdout.read().strip()
     proc.stdout.close()
     return output
@@ -36,13 +34,13 @@ class BurpExtender(IBurpExtender, ISessionHandlingAction):
     # stderr = PrintWriter(callbacks.getStderr(), True)
     self._callbacks = callbacks
     self._helpers = callbacks.getHelpers()
-    callbacks.setExtensionName("External Crypto Header")
+    callbacks.setExtensionName("Custom External Crypto Headers")
     callbacks.registerSessionHandlingAction(self)
 
     return
 
   def getActionName(self):
-    return "External Crypto Header"
+    return "Custom External Crypto Headers"
 
   def performAction(self, currentRequest, macroItems):
     requestInfo = self._helpers.analyzeRequest(currentRequest)
@@ -50,56 +48,60 @@ class BurpExtender(IBurpExtender, ISessionHandlingAction):
     newHeaders = list(headers)
 
     reqBody = currentRequest.getRequest()[requestInfo.getBodyOffset():]
-    parameters = self._helpers.bytesToString(reqBody)
+    body_parameters = self._helpers.bytesToString(reqBody)
 
-    C1_key = "API_KEY"
+    api_key = "XXX"
     timestamp = self.current_milli_time()
-    ContentMD5 = ""
-    ContentType = ""
+    content_md5 = ""
+    content_type = ""
     api_endpoint = urlparse.urlparse(str(currentRequest.getUrl())).path
-    getParameters = sorted(requestInfo.getParameters())
-    
-    # Some hack way to get parameters and sort e.g. https://domain.com/?a=first&c=third&b=second will give a=first&b=second&c=third
-    # a lot of weird edge cases
-    if getParameters:
-      temp=[]
-      for parameter in getParameters:
-        temp.append(parameter.getName() + '=' + parameter.getValue() + '&')
-      api_parameters = (''.join(sorted(temp))[:-1])
-      api_endpoint = api_endpoint + '?' + api_parameters
+
+    # Get parameters in URL (for some reason POST data would be included, so we specify for only GET)
+    for header in newHeaders:
+      if (header.lower().startswith("get")):
+        get_parameters = sorted(requestInfo.getParameters())
+
+        # Some hack way to get parameters and sort e.g. https://domain.com/?a=first&c=third&b=second will give a=first&b=second&c=third
+        # a lot of weird edge cases
+        if get_parameters:
+          temp=[]
+          for parameter in get_parameters:
+            temp.append(parameter.getName() + '=' + parameter.getValue() + '&')
+          api_parameters = (''.join(sorted(temp))[:-1])
+          api_endpoint = api_endpoint + '?' + api_parameters
+
     # Remove old HTTP headers
     for header in newHeaders:
-      if (header.startswith("Content-Type")):
+      if (header.lower().startswith("content-type")):
         headers.remove(header)
-      if (header.startswith("X-Signature")):
+      if (header.lower().startswith("x-signature")):
         headers.remove(header)
-      if (header.startswith("X-APIkey")):
+      if (header.lower().startswith("x-apikey")):
         headers.remove(header)
-      if (header.startswith("X-Timestamp")):
+      if (header.lower().startswith("x-timestamp")):
         headers.remove(header)
-      if (header.startswith("Content-MD5")):
+      if (header.lower().startswith("content-md5")):
         headers.remove(header)
 
     # Hacky method (no regex) to check if GET or POST/PUT HTTP method e.g. if headers has GETTEST it will be a valid GET too
     for header in newHeaders:
-      if (header.startswith("GET")):
-        # headers.add(test)
+      if (header.lower().startswith("get")):
         # headers.add('Content-Type: text/plain')
-        StringToSign = "GET" + "\n" + timestamp + "\n" + ContentMD5 + "\n"+ ContentType + "\n" + C1_key + "\n" + api_endpoint
-      if (header.startswith("POST")):
-        ContentType = "application/json"
-        headers.add('Content-Type: ' + ContentType)
-        ContentMD5 = hashlib.md5(parameters).hexdigest()
-        headers.add('Content-MD5: ' + ContentMD5)
-        StringToSign = "POST" + "\n" + timestamp + "\n" + ContentMD5 + "\n"+ ContentType + "\n" + C1_key + "\n" + api_endpoint
-      if (header.startswith("PUT")):
-        ContentType = "application/json"
-        headers.add('Content-Type: ' + ContentType)
-        ContentMD5 = hashlib.md5(parameters).hexdigest()
-        headers.add('Content-MD5: ' + ContentMD5)
-        StringToSign = "PUT" + "\n" + timestamp + "\n" + ContentMD5 + "\n"+ ContentType + "\n" + C1_key + "\n" + api_endpoint
+        StringToSign = "GET" + "\n" + timestamp + "\n" + content_md5 + "\n"+ content_type + "\n" + api_key + "\n" + api_endpoint
+      if (header.lower().startswith("post")):
+        content_type = 'application/json'
+        headers.add('Content-Type: ' + content_type)
+        content_md5 = hashlib.md5(body_parameters).hexdigest()
+        headers.add('Content-MD5: ' + content_md5)
+        StringToSign = "POST" + "\n" + timestamp + "\n" + content_md5 + "\n"+ content_type + "\n" + api_key + "\n" + api_endpoint
+      if (header.lower().startswith("put")):
+        content_type = 'application/json'
+        headers.add('Content-Type: ' + content_type)
+        content_md5 = hashlib.md5(body_parameters).hexdigest()
+        headers.add('Content-MD5: ' + content_md5)
+        StringToSign = "PUT" + "\n" + timestamp + "\n" + content_md5 + "\n"+ content_type + "\n" + api_key + "\n" + api_endpoint
 
-    headers.add(StringToSign)
+    # headers.add(StringToSign)
     # Call external program to run python program. Uses locally installed pycryptodome cryptographic signing functions
     proc = subprocess.Popen(['py',"./sign.py", StringToSign],stdout=subprocess.PIPE)
     output = proc.stdout.read().strip()
@@ -107,7 +109,7 @@ class BurpExtender(IBurpExtender, ISessionHandlingAction):
 
     headers.add("X-Signature: " + output)
     headers.add("X-Timestamp: " + timestamp)
-    headers.add("X-APIkey: " + C1_key)
+    headers.add("X-APIkey: " + api_key)
 
     # Build request with new headers
     message = self._helpers.buildHttpMessage(headers, reqBody)
